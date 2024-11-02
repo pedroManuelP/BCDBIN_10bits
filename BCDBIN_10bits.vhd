45f5ca5879b7e4c8f93afb4bab447ffdebfa2b09
Pedro Manuel <112979257+pedroManuelP@users.noreply.github.com>
2024-11-02 15:55:41 -0300

    Update BCDBIN_10bits.vhd

--- BCDBIN_10bits.vhd
+++ BCDBIN_10bits.vhd
@@ -27,29 +27,26 @@ architecture Hardware of BCDBIN_10bits is
 		);
 	end component;
 
-	component somador10bits is
+	component Somador10bits is
 		port(
 			A,B: in std_logic_vector (9 downto 0);
-			CIN: in std_logic;
+			cin: in std_logic;
 			S: out std_logic_vector (9 downto 0);
-			COUT: out std_logic
 		);
 	end component;
 	
 	signal centena, dezena, unidade: std_logic_vector(9 downto 0);
 	
 	signal soma1: std_logic_vector(9 downto 0);
-	signal cout1: std_logic;
 	signal soma2: std_logic_vector(9 downto 0);
-	signal cout2: std_logic;
 	
 begin
 	bcd_centena: BCDBIN_Centena port map(BCD(11 downto 8), centena);
 	bcd_dezena: BCDBIN_Dezena port map(BCD(7 downto 4), dezena);
 	unidade <= "000000"  & BCD(3 downto 0);
 	
-	somador1: somador10bits port map(centena, dezena, '0', soma1, cout1);
-	somador2: somador10bits port map(soma1, unidade, cout1, soma2, cout2);
+	somador1: Somador10bits port map(centena, dezena, '0', soma1);
+	somador2: Somador10bits port map(soma1, unidade, '0', soma2);
 	
 	SW <= soma2;
 end Hardware;
